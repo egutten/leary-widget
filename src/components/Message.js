afddc5c1801161ae92e2e627ef7726f6cc68317d
egutten <guttenberg.emily@gmail.com>
2019-09-27 12:44:03 -0700

    minor cleanup -- getting rid of some errors

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,17 +1,30 @@
-import React from 'react'; 
-
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const message = (props) => (
-  
   <div className={props.show}>
     <div>
-        <img className="logo" src={props.logo}/>
+      <img className="logo" alt="logo" src={props.logo} />
     </div>
     <div className="text">
-      <p className="event-text">{props.conversionEvent + "!"}</p>
+      <p className="event-text">{`${props.conversionEvent}!`}</p>
       <p className="time-stamp">{props.timestamp}</p>
     </div>
-  </div> 
+  </div>
 );
+
+message.propTypes = {
+  logo: PropTypes.string,
+  show: PropTypes.string,
+  conversionEvent: PropTypes.string,
+  timestamp: PropTypes.string,
+};
+
+message.defaultProps = {
+  logo: null,
+  show: null,
+  conversionEvent: null,
+  timestamp: null,
+};
 
 export default message;
--- src/components/widget.js
+++ src/components/widget.js
@@ -1,116 +1,104 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import './widget.scss';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
-import Message from './Message.js';
+import Message from './Message';
 
 const cookies = new Cookies();
 
-var cookieJar = cookies.get('customer_id');
+const cookieJar = cookies.get('customerId');
+
+const url = window.location.href.indexOf('localhost') ? 'http://localhost:8080/' : 'https://getleery.com';
 
 class Widget extends Component {
   state = {
-    show: "message-box hidden",
+    show: 'message-box hidden',
     conversion_event_text: null,
     timestamp: null,
     logo: null,
-    customer_id: null
+    customer_id: null,
+    position: null,
   }
   
   componentDidMount() {
-    var that = this;
-    
+    const that = this;
     function createCustomer() {
-      return axios.post("http://localhost:8080/customer-activity",{
+      return axios.post(`${url}customer-activity`, {
         user_id: that.props.userId,
-        event: 'view'
-      }).then(response => {
-        return response;
-      })
-    };
-    
+        event: 'view',
+      }).then((response) => response);
+    }
     function setCustomerIdFromState(customer_id) {
-      return new Promise((resolve, reject) => {
-        cookies.set('customer_id', customer_id.data, {path: '/', expires: new Date(Date.now() + 2592000)});
-        that.setState({customer_id: customer_id.data}, () => {
-          axios.post("http://localhost:8080/messages", {
+      return new Promise((resolve) => {
+        cookies.set('customerId', customer_id.data, { path: '/', expires: new Date(Date.now() + 2592000) });
+        that.setState({ customer_id: customer_id.data }, () => {
+          axios.post(`${url}messages`, {
             user_id: that.props.userId,
-            customer_id: that.state.customer_id
-          }).then(response => {
+            customer_id: that.state.customer_id,
+          }).then((response) => {
             resolve(response);
-          })
-        });  
+          });
+        });
       });
-    };  
-    
+    }
     function setCustomerIdFromCookie() {
-      return axios.post("http://localhost:8080/messages", {
+      return axios.post(`${url}messages`, {
         user_id: that.props.userId,
-        customer_id: cookies.get('customer_id')
-      }).then(response => {
-        return response;
-      });
-    };
-    
+        customer_id: cookies.get('customer_id'),
+      }).then((response) => response);
+    }
     function setMessageDataToState(message) {
       that.setState({
         conversion_event_text: message.data.conversion_event,
-        logo: "//logo.clearbit.com/" + message.data.logo,
-        timestamp: message.data.timestamp
+        logo: `//logo.clearbit.com/${message.data.logo}`,
+        timestamp: message.data.timestamp,
+        position: message.data.position,
       });
-    };
-    
+    }
     if (cookieJar === undefined) {
       createCustomer()
-      .then((customer_id) => {
-        setCustomerIdFromState(customer_id)
+       .then((customer_id) => {
+          setCustomerIdFromState(customer_id)
+            .then((message) => {
+              setMessageDataToState(message);
+            });
+        });
+    } else {
+      setCustomerIdFromCookie()
         .then((message) => {
           setMessageDataToState(message);
         });
-      });
-    } else {
-      setCustomerIdFromCookie()
-      .then((message) => {
-        setMessageDataToState(message);
-      });
-    };
-
+    }
     setTimeout(() => {
-      this.setState({show: "message-box entering" + " " + this.props.position});
-    }, 2000)
-    
+      this.setState({ show: `message-box entering ${this.state.position}` });
+    }, 2000);
     setTimeout(() => {
-      this.setState({show: "message-box exiting" + " " + this.props.position});
+      this.setState({ show: `message-box exiting ${this.state.position}` });
     }, 6000);
-  }  
+  }
   
   render() {
     let message = null;
     if (this.state.conversion_event_text !== null) {
-      message = <Message 
-        show = {this.state.show}
-        conversionEvent = {this.state.conversion_event_text} 
-        timestamp = {this.state.timestamp}
-        logo = {this.state.logo} />;
-    };
-    
+      message = (
+        <Message
+          show={this.state.show}
+          conversionEvent={this.state.conversion_event_text}
+          timestamp={this.state.timestamp}
+          logo={this.state.logo}
+        />
+      );
+    }
     return (
       <React.Fragment>
-       {message}
-     </React.Fragment>
+        {message}
+      </React.Fragment>
     );
   }
 }
 
-Widget.propTypes = {
-  userId: PropTypes.number,
-  position: PropTypes.string
-};
-
 Widget.defaultProps = {
   userId: 0,
-  position: null
 };
 
 export default Widget;
